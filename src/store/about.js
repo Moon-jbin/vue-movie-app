87a7fec465ff03b5165584796e71629c11f4877c
Moon-jbin <mjb1209@naver.com>
2021-06-21 23:30:22 +0900

    image 수정

--- src/store/about.js
+++ src/store/about.js
@@ -6,7 +6,7 @@ export default {
       email: 'mjb1209@naver.com',
       blog: 'https://blog.naver.com/mjb1209',
       phone: '+82-10-5093-9512',
-      image: '../../jjong.jpg'
+      image: '../../jjong/jjong.jpg'
 
     }
   }
