861ba1bf15407346ba3e2ac7a6631faf2a263118
Moon-jbin <mjb1209@naver.com>
2021-06-22 11:07:29 +0900

    실험

--- src/store/about.js
+++ src/store/about.js
@@ -6,7 +6,7 @@ export default {
       email: 'mjb1209@naver.com',
       blog: 'https://blog.naver.com/mjb1209',
       phone: '+82-10-5093-9512',
-      image: 'https://cdn.pixabay.com/photo/2017/12/25/10/47/toy-3038204_960_720.jpg'
+      image: '../../logo.png'
 
     }
   }
